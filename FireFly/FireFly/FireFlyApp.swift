926e2e354b2e06cf63872021a192960674f6bd78
숩 <realhsb@gmail.com>
2025-03-24 14:44:40 +0900

    프로젝트 파일 생성

---
+++ FireFly/FireFly/FireFlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireFlyApp.swift
+//  FireFly
+//
+//  Created by Soop on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FireFlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FireFly/FireFlyTests/FireFlyTests.swift
@@ -0,0 +1,16 @@
+//
+//  FireFlyTests.swift
+//  FireFlyTests
+//
+//  Created by Soop on 3/24/25.
+//
+
+import Testing
+
+struct FireFlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
